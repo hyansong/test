bb783431af4a64958c429964d576944eda5e1b4b
houyansong <houyansong@adasplus.com>
2021-10-19 12:01:25 +0800

    ce

---
+++ src/main/java/com/magic/util/FUtil.java
@@ -0,0 +1,16 @@
+package com.magic.util;
+
+import com.magic.test.MagicInfo;
+
+public class FUtil {
+
+    public static <T> T getBean(Class<T> tClass) throws IllegalAccessException, InstantiationException {
+        T t = tClass.newInstance();
+        return t;
+    }
+
+    public static void main(String[] args) throws Exception{
+        MagicInfo bean = FUtil.getBean(MagicInfo.class);
+        System.out.println(bean.getKey());
+    }
+}
